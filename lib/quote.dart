e9cd160c473c537eb6bacacd79026d40c88be92f
skillsapphire <skillsapphire@gmail.com>
2020-01-04 12:39:13 +0100

    first commit

---
+++ lib/quote.dart
@@ -0,0 +1,12 @@
+class Quote{
+  String text;
+  String author;
+
+  // named parameters, takes the value and assign it , no need of sequence
+  Quote({this.text, this.author});
+
+  /*Quote(String text, String author){
+    this.text = text;
+    this.author = author;
+  }*/
+}
